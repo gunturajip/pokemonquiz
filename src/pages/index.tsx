0275fdb5daa75764de10613e5187c74bc28be9cb
gunturajip <gunturaji852@gmail.com>
2022-07-23 23:57:15 +0700

    adding helper implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
+import { getOptionsforVote } from "../utils/getRandomPokemon";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from PokeAPI" }]);
+  const [first, second] = getOptionsforVote();
 
   return (
     <>
@@ -10,9 +12,9 @@ const Home: NextPage = () => {
         <div className="text-2xl text-center">Which Pokemon is Rounder? <div className="text-base">{hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}</div></div>
         <div className="p-2" />
         <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-          <div className="w-16 h-16 bg-red-200" />
+          <div className="w-16 h-16 bg-teal-800 justify-center items-center flex">{first}</div>
           <div className="p-8">Vs</div>
-          <div className="w-16 h-16 bg-red-200" />
+          <div className="w-16 h-16 bg-teal-800 justify-center items-center flex">{second}</div>
         </div>
       </div>
     </>
